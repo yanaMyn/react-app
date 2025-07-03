c2281baf70828891f53e18220e2570b772200d14
Yana Mulyana <yanamyn15@gmail.com>
2025-07-03 21:44:01 +0700

    install bootstrap & jsx condition

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+// function Message() {
+// const name = "Yana"
+
+//     // JSX: JavaSript XML
+//     return <h1>Hello world! {name}</h1>
+// }
+
+// export default Message
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,18 @@
+function ListGroup() {
+  let items = ["New York", "Las Vegas", "Miami", "Washington"];
+//   items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No Item Found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
